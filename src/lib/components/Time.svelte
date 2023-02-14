7b480dfd69d2c3e88f0888e94cdb507362e17454
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-14 11:32:06 -0800

    :sparkles: Adds time component

---
+++ src/lib/components/Time.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import dayjs from 'dayjs';
+
+	let time = '';
+	let date = '';
+	const timeFormat = 'h:mm:ss';
+	const dateFormat = 'ddd MMM D, YYYY';
+
+	function update() {
+		time = dayjs().format(timeFormat);
+		date = dayjs().format(dateFormat);
+	}
+
+	onMount(() => {
+		update();
+		setInterval(() => {
+			update();
+		}, 500);
+	});
+</script>
+
+<div class="flex items-center justify-center flex-col">
+	<p class="text-4xl p-0 m-0 text-gray-100">It's</p>
+	<p class="text-4xl p-0 m-0 text-gray-200">{time}</p>
+	<p class="text-3xl p-0 m-0 text-gray-300">{date}</p>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		color: theme(colors.gray.100);
+		background-color: theme(colors.gray.800);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
