2184fafe43877a52bc2dadd0f56bf3aa1a1719cd
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-14 12:48:01 -0800

    :lipstick: Fixes search UI

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,7 +1,19 @@
-<div class="flex items-center justify-center p-4 w-fit border-none rounded-full bg-neutral-700">
-	<a href="https://www.google.com" class="w-8 h-8 inline-block">
-		<img src="/google.png" alt="Google" class="" />
+<script>
+	let value = '';
+</script>
+
+<div
+	class="flex items-center justify-center p-3 w-96 rounded-full bg-neutral-800 border border-solid border-neutral-700"
+>
+	<a href="https://www.google.com" class="inline-block">
+		<img src="/google.png" alt="Google" class="w-8 h-8" />
 	</a>
-    <input class="bg-inherit border-b-1 border-neutral-100 border-solid mx-4">
-	<span class="material-symbols-outlined text-3xl">search</span>
+	<input
+		placeholder="Search"
+		bind:value
+		class="bg-inherit border-b-1 border-neutral-100 border-solid mx-4 focus:outline-none focus:border-none text-xl"
+	/>
+	<a href="https://www.google.com/search?q={value}">
+		<span class="material-symbols-outlined text-3xl w-8 h-8">search</span>
+	</a>
 </div>
