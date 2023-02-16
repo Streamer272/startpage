2b19bc9701871d41f62b529d9e7266f6e63786c1
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-16 07:56:49 -0800

    :zap: Adds shortcut tile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,24 @@
 	import type { Row } from '$lib/types/tile';
 	import { newShortcut, newWeather } from '$lib/types/tile';
 
-	const rows: Row[] = [[newWeather(), newShortcut(), newShortcut()]];
+	const rows: Row[] = [
+		[
+			newWeather(),
+			newShortcut(
+				'https://reddit.com',
+				'Reddit',
+				'https://reddit.com/favicon.ico',
+				'https://new.svitan.dev',
+				'New',
+				'https://new.svitan.dev/favicon.ico'
+			),
+			newShortcut(
+				'https://console.firebase.google.com',
+				'Firebase',
+				'http://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_96dp.png'
+			)
+		]
+	];
 </script>
 
 <div class="flex items-center justify-center flex-col">
@@ -22,12 +39,36 @@
 	{/if}
 	<div class="h-10" />
 	{#each rows as row}
-		<div class="flex items-center justify-center">
+		<div class="flex items-center justify-center mb-6 last:mb-0">
 			{#each row as tile}
 				{#if tile.type === 'Weather'}
 					<Weather />
 				{:else if tile.type === 'Shortcut'}
-					<Tile>Shortcut</Tile>
+					<Tile>
+						{#if tile.link1}
+							<a
+								href={tile.link1}
+								class="flex items-center justify-center flex-row text-inherit visited:text-inherit"
+							>
+								{#if tile.icon1}
+									<img src={tile.icon1} alt={tile.title1} class="w-8 h-8" />
+								{/if}
+								<p class="ml-2">{tile.title1}</p>
+							</a>
+						{/if}
+						{#if tile.link2}
+							<div class="h-2" />
+							<a
+								href={tile.link2}
+								class="flex items-center justify-center flex-row text-inherit visited:text-inherit"
+							>
+								{#if tile.icon2}
+									<img src={tile.icon2} alt={tile.title2} class="w-8 h-8" />
+								{/if}
+								<p class="ml-2 text-lg">{tile.title2}</p>
+							</a>
+						{/if}
+					</Tile>
 				{/if}
 			{/each}
 		</div>
