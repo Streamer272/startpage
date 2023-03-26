6b8420db31a8e40b8605b576be467045e8c43852
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-03-25 23:03:36 -0700

    :sparkles: Makes input into a form

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -4,10 +4,23 @@
 
 	let value = '';
 
+	function submit(event: Event) {
+		event.preventDefault();
+		window.location.assign(`https://www.google.com/search?q=${value}`);
+	}
+
 	onMount(() => {
 		hotkeys('ctrl+k', (event) => {
 			event.preventDefault();
 			document.querySelector('input')?.focus();
+		});
+		hotkeys('ctrl+return', (event) => {
+			event.preventDefault();
+			window.open(`https://www.google.com/search?q=${value}`, '_blank');
+		});
+		hotkeys('return', (event) => {
+			event.preventDefault();
+			window.location.assign(`https://www.google.com/search?q=${value}`);
 		});
 	});
 </script>
@@ -18,11 +31,13 @@
 	<a href="https://www.google.com" class="">
 		<img src="/google.png" alt="Google" class="w-8 h-8" />
 	</a>
-	<input
-		placeholder="Search"
-		bind:value
-		class="bg-inherit border-b-1 border-neutral-100 border-solid mx-4 focus:outline-none focus:border-none text-xl"
-	/>
+	<form on:submit={submit}>
+		<input
+			placeholder="Search"
+			bind:value
+			class="bg-inherit border-b-1 border-neutral-100 border-solid mx-4 focus:outline-none focus:border-none text-xl"
+		/>
+	</form>
 	<a href="https://www.google.com/search?q={value}" class="visited:text-inherit">
 		<span class="material-symbols-outlined text-3xl w-8 h-8 visited:text-inherit">search</span>
 	</a>
